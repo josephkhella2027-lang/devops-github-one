17bfb774f56be7d64424200013718eab85e48548
joseph khella <josephkhella2010@gmail.com>
2026-05-16 22:44:36 +0200

    fix inuts

--- frontend/train/src/pages/HomePage/HomePage.tsx
+++ frontend/train/src/pages/HomePage/HomePage.tsx
@@ -1,14 +1,13 @@
-
 function HomePage() {
   return (
     <div>
-        <h1>Home Page</h1>
-        <input type="text" placeholder="email" />
-        <input type="text" placeholder="password" />
-
-      
+      <h1>Home Page</h1>
+      <input type="text" placeholder="email" />
+      <input type="text" placeholder="password" />
+      <input type="text" placeholder="username" />
+      <input type="text" placeholder="username" />
     </div>
-  )
+  );
 }
 
-export default HomePage
+export default HomePage;
