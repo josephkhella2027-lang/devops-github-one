cb9e79e2a1c36c62d343ee92992e1c8bcf61b566
joseph khella <josephkhella2010@gmail.com>
2026-05-17 17:03:25 +0200

    fix git ignore

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import getUsers from "./api-routes/getUser.js";
+import dotenv from "dotenv";
+
+dotenv.config();
 const app = express();
 app.use(express.json());
-
-const PORT = 5300;
 app.use("/api", getUsers);
 
-app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
-});
+const PORT = process.env.PORT || 5300;
+
+// ONLY start server outside tests
+if (process.env.NODE_ENV !== "test") {
+  app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+  });
+}
 
 export default app;
